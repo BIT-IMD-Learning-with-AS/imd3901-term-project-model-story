dfca25bbcf860f93a18c319c67f68f0efeed854a
Chris <chrishearty2@gmail.com>
2022-02-27 20:07:51 -0500

    Added Basic Pickup
    -Added pickup
    -Also tried Super-Hands but isn't working right now

---
+++ public/js/pickup.js
@@ -0,0 +1,44 @@
+'use strict'
+
+AFRAME.registerComponent('pickup', {
+    schema : {
+        //list properties
+    },
+    init : function(){
+        //called when program starts
+        //only called once AFRAME is built
+
+        const CONTEXT_AF        = this; // 'this' is this component
+        CONTEXT_AF.camera       = document.querySelector('#camera');
+        CONTEXT_AF.scene        = document.querySelector('#scene');
+        let pickedUp            = false;
+        let clone;
+
+        CONTEXT_AF.el.addEventListener('click', function() {
+
+            if(this.pickedUp === true) {
+                //drop
+                clone = this.cloneNode(true);
+                clone.pickedUp = false;
+                CONTEXT_AF.scene.appendChild(clone);
+                this.parentNode.removeChild(this);
+                clone.setAttribute('position', {x: CONTEXT_AF.camera.object3D.position.x, z: CONTEXT_AF.camera.object3D.position.z});
+                //Idk if everything is gonna have physics. I assume it will if its an interactable element.
+                clone.setAttribute('dynamic-body', '')
+
+                console.log("Dropped");
+            }
+            else{
+                //pickup
+                clone = this.cloneNode(true);
+                clone.pickedUp = true;
+                CONTEXT_AF.camera.appendChild(clone);
+                this.parentNode.removeChild(this);
+                clone.setAttribute('position', {x: 0.7, y: -0.4, z: -1})
+
+                console.log("Picked Up");
+            }
+        })
+
+    }
+})
