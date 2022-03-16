6b7c904006ff8555418405e055cd32679b3fc309
Chris <chrishearty2@gmail.com>
2022-03-16 17:42:51 -0400

    Fixed Pickup and Placement
    - Fixed Pickup and Placement
    - Deleted Placement js
    - Cleaned Pickup js

--- public/js/pickup.js
+++ public/js/pickup.js
@@ -3,7 +3,8 @@
 AFRAME.registerComponent('pickup', {
     schema : {
         //list properties
-        pickedUp: {type: 'bool', default: false}
+        pickedUp: {type: 'bool', default: false},
+        objVal: {type: 'int', default: 0}
     },
     init : function(){
         //called when program starts
@@ -12,36 +13,65 @@ AFRAME.registerComponent('pickup', {
         const CONTEXT_AF        = this; // 'this' is this component
         CONTEXT_AF.camera       = document.querySelector('#camera');
         CONTEXT_AF.scene        = document.querySelector('#scene');
-        //let pickedUp            = false;
+
+        CONTEXT_AF.placement1 = document.querySelector('#placement1');
+        let placePos1X = CONTEXT_AF.placement1.object3D.position.x;
+        let placePos1Z = CONTEXT_AF.placement1.object3D.position.z;
+
         let clone;
+        let objPosX;
+        let objPosZ;
+
+        let reachDist = 2;
 
         CONTEXT_AF.el.addEventListener('click', function() {
-
+            //Check if picked up
             if(this.pickedUp === true) {
-                //drop
-                clone = this.cloneNode(true);
-                clone.pickedUp = false;
-                CONTEXT_AF.scene.appendChild(clone);
-                this.parentNode.removeChild(this);
-                clone.setAttribute('position', {x: CONTEXT_AF.camera.object3D.position.x, z: CONTEXT_AF.camera.object3D.position.z});
-                //Idk if everything is gonna have physics. I assume it will if its an interactable element.
-                clone.setAttribute('dynamic-body', '')
+                //Get Positions of object
+                objPosX = CONTEXT_AF.camera.object3D.position.x;
+                objPosZ = CONTEXT_AF.camera.object3D.position.z;
+ 
+                if(this.getAttribute("pickup").objVal == 1 && Math.abs(objPosX - placePos1X) < reachDist && Math.abs(objPosZ - placePos1Z) < reachDist){
+                    //If Object 1 and near Placement
 
-                console.log("Dropped");
+                    //Clone object + add to scene
+                    clone = this.cloneNode(true);
+                    clone.pickedUp = false;
+                    CONTEXT_AF.scene.appendChild(clone);
+                    this.parentNode.removeChild(this);
+
+                    //Set Position to Placement
+                    clone.setAttribute('position', {x: CONTEXT_AF.placement1.object3D.position.x, y: CONTEXT_AF.placement1.object3D.position.y, z: CONTEXT_AF.placement1.object3D.position.z});
+                }
+                else{
+                    //Drop
+
+                    //Clone object + add to scene
+                    clone = this.cloneNode(true);
+                    clone.pickedUp = false;
+                    CONTEXT_AF.scene.appendChild(clone);
+                    this.parentNode.removeChild(this);
+
+                    //Set Position to camera
+                    clone.setAttribute('position', {x: CONTEXT_AF.camera.object3D.position.x, z: CONTEXT_AF.camera.object3D.position.z});
+                    
+                    //Not sure if everything is going to have physics. I assume it will if its an interactable element.
+                    //dynamic-body needs to be re-added
+                    clone.setAttribute('dynamic-body', '')
+                }
             }
             else{
-                //pickup
+                //Pickup
+
+                //Clone object + add to camera
                 clone = this.cloneNode(true);
                 clone.pickedUp = true;
                 CONTEXT_AF.camera.appendChild(clone);
                 this.parentNode.removeChild(this);
-                clone.setAttribute('position', {x: 0.6, y: -0.6, z: -0.8})
-                
-                console.log("Picked Up");
+
+                //Set Position on camera
+                clone.setAttribute('position', {x: 0.6, y: -0.6, z: -0.8});
             }
         })
-
-        
-
     }
 })
