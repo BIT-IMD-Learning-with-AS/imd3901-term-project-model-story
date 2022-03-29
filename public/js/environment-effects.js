d6322559e99ba95084c8c2134acdcdfad99739d3
ewaldram <60678943+ewaldram@users.noreply.github.com>
2022-03-29 19:49:18 -0400

    Smol changes

--- public/js/environment-effects.js
+++ public/js/environment-effects.js
@@ -6,29 +6,43 @@ AFRAME.registerComponent('environment-effect', {
     init : function () {
         //This objectives array is where we store completed puzzles. 
         let objectives = [0,0,0,0,0];
+        let sound = document.getElementById("sound-fx");
 
         //0: if the land bridge puzzle is complete
         if (objectives[0] === 1)
         {
             //swap whatever landbridge model for another gltf.
+            //THIS IS HYPOTHETICAL AS THE BRIDGE MODELS ARENT SET UP YET
+            //document.getElementById('landBridge').setAttribute('gltf-model',"#landBridge_model")
         }
 
         //1: if the watering can puzzle is complete
         if (objectives[1] === 1)
         {
+            sound.setAttribute('sound', {src:'#squeak_sfx'});
             //basically just add a metric butt-ton of PLANTS
             //either have them pre-placed and load them, or just spawn them similar to A2.
+            //entirely dependant on the outdoorsy layout, so I can't have it written out yet, but general idea is as follows:
+            
+            //const folliage = document.createElement("a-entity");
+            //folliage.setAttribute('class', 'folliage');
+            //folliage.setAttribute('position', {x: 0, y:0.2, z:0});
+            //folliage.setAttribute('gltf-model', '#folliage_model');
+
+            //REPEAT THE ABOVE CODE AS MUCH AS NEEDED FOR OUR GREENS
         }
 
         //2: sewage puzzle
         if (objectives[2] === 1)
         {
+            sound.setAttribute('sound', {src:'#squeak_sfx'});
             //change the colour of the object, or load in a second model
         }
 
         //3: stars
         if (objectives[3] === 1)
         {
+            sound.setAttribute('sound', {src:'#squeak_sfx'});
             //change the background.
             //might have to rebuild the environment at this point.
         }
@@ -36,6 +50,7 @@ AFRAME.registerComponent('environment-effect', {
         //4: Crickets puzzle
         if (objectives[4] === 1)
         {
+            sound.setAttribute('sound', {src:'#squeak_sfx'});
             //Easiest effects to change because it's just swapping sound effects.
             document.getElementsByClassName('ambient-music').setAttribute('sound', {src:'#bird_amb'});
         }
@@ -43,22 +58,10 @@ AFRAME.registerComponent('environment-effect', {
         //Check all
         if (objectives === [1,1,1,1,1])
         {
+            sound.setAttribute('sound', {src:'#squeak_sfx'}); //figure out how to restart play and how to loop
             //Trigger Win Screen
             //Take code from intro screen and just rework it
 
         }
-
-       // const CONTEXT_AF = this; //this refers to 'this' component, because 'this' in an event listener refers to something else.
-        
-        //CONTEXT_AF.el.addEventListener('click', function() {
-          //  if (bridge === true) {
-                //swap scene to the landbridge
-            //}
-            //else {
-                //bridgeless scene
-            //}
-        //});
-   
-   
     }
 });
