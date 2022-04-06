2ba295199882c21df47dce5b974a808439d296c4
ewaldram <60678943+ewaldram@users.noreply.github.com>
2022-04-06 16:39:02 -0400

    land bridge change

--- public/js/environmental-effects.js
+++ public/js/environmental-effects.js
@@ -1,14 +1,15 @@
 'use strict'
+
 //Land Bridge
-AFRAME.registerComponent('landBridge-effect', {
+AFRAME.registerComponent('bridge-effect', {
     init : function () {
         const CONTEXT_AF = this;
         CONTEXT_AF.el.addEventListener('click', function() {
-            //Effectively just changing one asset for another.
-            //this is a test, as I dont have the proper assets yet.
-            console.log("Landbridge changed.");
-            //document.getElementById("fridge").removeAttribute('gltf-model');
-            //document.getElementById("fridge").setAttribute('gltf-model', '#kSE_model')
+            //make land bridge visible.
+            const bridges = document.querySelectorAll(".landBridge");
+            for (let i = 0; i < bridges.length; i++) {
+                bridges[i].setAttribute('visible', true);
+            } 
         });
     }
 });
@@ -20,7 +21,6 @@ AFRAME.registerComponent('birds-effect', {
          const CONTEXT_AF = this;
          CONTEXT_AF.el.addEventListener('click', function() {
              //Changing city sounds to the birds.
-             //console.log("Music changed.");
              document.getElementById("city_sounds").pause();
              document.getElementById("bird_sounds").components.sound.playSound();
          });
@@ -43,9 +43,6 @@ AFRAME.registerComponent('stars-effect', {
  });
  //Plantlife grows
 AFRAME.registerComponent('plant-effect', {
-    schema : {
-        
-     },
      init : function () {
          const CONTEXT_AF = this;
          CONTEXT_AF.el.addEventListener('click', function() {
@@ -58,9 +55,6 @@ AFRAME.registerComponent('plant-effect', {
 
  //River Changes
 AFRAME.registerComponent('river-effect', {
-    schema : {
-        
-     },
      init : function () {
          const CONTEXT_AF = this;
          CONTEXT_AF.el.addEventListener('click', function() {
