a1426c03ba4fcbbe46bdf481aebf1aa7a952b6b1
Gavin Isaac <vin.isaac01@gmail.com>
2022-04-07 18:21:02 -0400

    Create gameMaster.js

---
+++ public/js/gameMaster.js
@@ -0,0 +1,22 @@
+'use strict'
+
+//Created from a Tutorial by Don McCurdy
+'use strict'
+
+AFRAME.registerComponent('game-master', {
+    init: function () {
+      const el = this.el;
+
+      var bugPuzzle = false;
+      var lightPuzzle = false;
+      var picPuzzle = false;
+      var plantPuzzle = false;
+      var cleanPuzzle = false;
+
+  
+      // On click, send the NPC to the target location.
+      el.addEventListener('click', (e) => {
+
+      });
+    }
+  });
