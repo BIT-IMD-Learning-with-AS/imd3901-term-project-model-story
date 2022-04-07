6378e62a10dceb23d594ab11d5eb2714c2519b29
ewaldram <60678943+ewaldram@users.noreply.github.com>
2022-04-07 14:07:46 -0400

    plant effect

--- public/js/environmental-effects.js
+++ public/js/environmental-effects.js
@@ -51,8 +51,62 @@ AFRAME.registerComponent('plant-effect', {
          const CONTEXT_AF = this;
          CONTEXT_AF.el.addEventListener('click', function() {
              //Plants growing
-             console.log("plants changed.");
+             const plants = document.querySelectorAll(".top-plants");
+             for (let i = 0; i < plants.length; i++) {
+                 plants[i].setAttribute('visible', true);
+             }
 
+             //change ground
+             console.log(document.getElementById('area_ground'));
+             document.getElementById("area_ground").removeAttribute('material');
+             document.getElementById('area_ground').setAttribute('material', 'color',  'green');
+
+             //generate 50 random trees.
+             for (var i = 0; i < 100; i++){
+                //randomly generate position and tree type.
+                var randX=Math.floor((Math.random() * 149) -49);
+                var randZ=Math.floor((Math.random() * 149) -49);
+                var randT=Math.floor(Math.random() * 7) + 1;
+
+                const tree = document.createElement("a-entity");
+
+                if (randT == 1)
+                {
+                    tree.setAttribute('gltf-model','#tree01');
+                }
+                if (randT == 2)
+                {
+                    tree.setAttribute('gltf-model','#tree02');
+                }
+                if (randT == 3)
+                {
+                    tree.setAttribute('gltf-model','#tree03');
+                }
+                if (randT == 4)
+                {
+                    tree.setAttribute('gltf-model','#tree04');
+                }
+                if (randT == 5)
+                {
+                    tree.setAttribute('gltf-model','#tree05');
+                }
+                if (randT == 6)
+                {
+                    tree.setAttribute('gltf-model','#tree06');
+                }
+                if (randT == 7)
+                {
+                    tree.setAttribute('gltf-model','#tree07');
+                }
+
+                tree.setAttribute('position', {x: randX, y:0.1, z:randZ});
+                tree.setAttribute('scale','7, 7, 7');
+
+                //add to scene
+                let trees = document.getElementById("trees")
+                trees.appendChild(tree)
+
+             }
          });
      }
  });
