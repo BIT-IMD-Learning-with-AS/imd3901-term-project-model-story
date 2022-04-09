60b2457821a601fead98fffdaa36fdd6e2255d6a
Chris <chrishearty2@gmail.com>
2022-04-08 20:13:04 -0400

    Added Painting Puzzle
    Setup paintings and hangars.

--- public/js/pickup.js
+++ public/js/pickup.js
@@ -17,12 +17,18 @@ AFRAME.registerComponent('pickup', {
         CONTEXT_AF.placement1 = document.querySelector('#placement1');
         let placePos1X = CONTEXT_AF.placement1.object3D.position.x;
         let placePos1Z = CONTEXT_AF.placement1.object3D.position.z;
+        CONTEXT_AF.placement2 = document.querySelector('#placement2');
+        let placePos2X = CONTEXT_AF.placement1.object3D.position.x;
+        let placePos2Z = CONTEXT_AF.placement1.object3D.position.z;
+        CONTEXT_AF.placement3 = document.querySelector('#placement3');
+        let placePos3X = CONTEXT_AF.placement1.object3D.position.x;
+        let placePos3Z = CONTEXT_AF.placement1.object3D.position.z;
 
         let clone;
         let objPosX;
         let objPosZ;
 
-        let reachDist = 2;
+        let reachDist = 10;
 
         CONTEXT_AF.el.addEventListener('click', function() {
             //Check if picked up
@@ -42,6 +48,30 @@ AFRAME.registerComponent('pickup', {
 
                     //Set Position to Placement
                     clone.setAttribute('position', {x: CONTEXT_AF.placement1.object3D.position.x, y: CONTEXT_AF.placement1.object3D.position.y, z: CONTEXT_AF.placement1.object3D.position.z});
+                }
+                else if(this.getAttribute("pickup").objVal == 2 && Math.abs(objPosX - placePos2X) < reachDist && Math.abs(objPosZ - placePos2Z) < reachDist){
+                    //If Object 2 and near Placement
+
+                    //Clone object + add to scene
+                    clone = this.cloneNode(true);
+                    clone.pickedUp = false;
+                    CONTEXT_AF.scene.appendChild(clone);
+                    this.parentNode.removeChild(this);
+
+                    //Set Position to Placement
+                    clone.setAttribute('position', {x: CONTEXT_AF.placement2.object3D.position.x, y: CONTEXT_AF.placement2.object3D.position.y, z: CONTEXT_AF.placement2.object3D.position.z});
+                }
+                else if(this.getAttribute("pickup").objVal == 3 && Math.abs(objPosX - placePos3X) < reachDist && Math.abs(objPosZ - placePos3Z) < reachDist){
+                    //If Object 3 and near Placement
+
+                    //Clone object + add to scene
+                    clone = this.cloneNode(true);
+                    clone.pickedUp = false;
+                    CONTEXT_AF.scene.appendChild(clone);
+                    this.parentNode.removeChild(this);
+
+                    //Set Position to Placement
+                    clone.setAttribute('position', {x: CONTEXT_AF.placement3.object3D.position.x, y: CONTEXT_AF.placement3.object3D.position.y, z: CONTEXT_AF.placement3.object3D.position.z});
                 }
                 else{
                     //Drop
@@ -70,7 +100,7 @@ AFRAME.registerComponent('pickup', {
                 this.parentNode.removeChild(this);
 
                 //Set Position on camera
-                clone.setAttribute('position', {x: 0.6, y: -0.6, z: -0.8});
+                clone.setAttribute('position', {x: 2, y: -2, z: -3});
             }
         })
     }
